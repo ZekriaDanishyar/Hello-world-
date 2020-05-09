f555073326b54d3618e1d7c2d583ce2e4b42442c
ZekriaDanishyar <danishyar.zekria@hotmail.com>
2020-05-08 23:43:20 -0400

    1- Jira Number => Automation120 -New Line of Code Added 2- Automtation Ticket # 3- Tasks

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -4,9 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebdriverSettings {
+	System.out.print("I love Java Programming");
 	
 	public WebDriver driverSettings() {
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe File PATH");
+		System.setProperty("webdriver.chrome.driver", 
+		"C:/Users/Zekria/eclipse-workspace/Training-June-2019/CucumberSelenium/src/test/resources/drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		return driver;
 	}
