46d87603d6e0343165dab1d137f907ec369a9a46
ZekriaDanishyar <danishyar.zekria@hotmail.com>
2020-08-30 18:42:17 -0400

    I have just changed the spelling from programming to program

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebdriverSettings {
-	System.out.print("I love Java Programming");
+	System.out.print("I love Java Program");
 	
 	public WebDriver driverSettings() {
 		System.setProperty("webdriver.chrome.driver", 
